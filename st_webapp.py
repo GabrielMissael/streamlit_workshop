e84cb489f0c0e3756490b3555bb6433f094c1b21
GabrielMissael <missaelgabo@gmail.com>
2023-02-23 21:31:24 -0600

    Add Python script with streamlit web app template

---
+++ st_webapp.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.markdown('''
+    # Mi primer web-app con Streamlit 👑.
+    -----
+
+    Tu tarea es crear una webapp que incluya el contenido de el notebook `digits_project.ipynb`
+    que se encuentra en el repositorio de github del taller.
+
+    Si quieres ver el código de una posible solución, ve a la rama `solution` del repositorio.
+
+    #### ¡Happy coding! 🚀
+''')
