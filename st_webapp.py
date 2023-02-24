85bf9529431cd3ffc380912b1fce32235effd90f
GabrielMissael <missaelgabo@gmail.com>
2023-02-23 21:33:48 -0600

    Update template

--- st_webapp.py
+++ st_webapp.py
@@ -4,8 +4,10 @@ st.markdown('''
     # Mi primer web-app con Streamlit 👑.
     -----
 
-    Tu tarea es crear una webapp que incluya el contenido de el notebook `digits_project.ipynb`
-    que se encuentra en el repositorio de github del taller.
+    Tu tarea es crear una web application usando Streamlit que incluya el contenido
+    del notebook `digits_project.ipynb` que se encuentra en el repositorio
+    de github del taller. ¡No olvides revisar la documentación de
+    [Streamlit](https://streamlit.io/)!
 
     Si quieres ver el código de una posible solución, ve a la rama `solution` del repositorio.
 
